4bee53b79973ca59cf6fb21f2bb63f99fbb24f13
grin3671 <grin3671@yandex.ru>
2025-08-18 23:03:11 +0300

    v1.5.0
    Added information to the tooltip of quest reward items
    Added support for multi-step achievements and criterion's list view
    Added all achievements from Trophies series
    Added few achievements from Skill Stylist series

--- KaleidoAchievementItems/KaleidoAchievementItems.lua
+++ KaleidoAchievementItems/KaleidoAchievementItems.lua
@@ -2,11 +2,63 @@ KaleidoAchievementItems = {}
 
 local this = KaleidoAchievementItems
 this.name = "KaleidoAchievementItems"
-this.version = "1.4.2"
+this.version = "1.5.0"
 this.author = "grin3671"
 
 -- this table contains itemsIds (string) and related achievementIds (integer)
-this.addonData = {
+local AddonData = {
+  ---- Trophies
+  ["54184"] = 838, -- Tamriel Beast Collector
+  ["54185"] = 838, -- Tamriel Beast Collector
+  ["54186"] = 838, -- Tamriel Beast Collector
+  ["54187"] = 838, -- Tamriel Beast Collector
+  ["54188"] = 838, -- Tamriel Beast Collector
+  ["54189"] = 838, -- Tamriel Beast Collector
+  ["54190"] = 838, -- Tamriel Beast Collector
+  ["54195"] = 838, -- Tamriel Beast Collector
+  ["54196"] = 838, -- Tamriel Beast Collector
+  ["54197"] = 838, -- Tamriel Beast Collector
+  ["54198"] = 838, -- Tamriel Beast Collector
+  ["54199"] = 841, -- Undead Hoarder
+  ["54200"] = 841, -- Undead Hoarder
+  ["54201"] = 841, -- Undead Hoarder
+  ["54202"] = 841, -- Undead Hoarder
+  ["54203"] = 841, -- Undead Hoarder
+  ["54204"] = 841, -- Undead Hoarder
+  ["54205"] = 842, -- Chitin Accumulator
+  ["54206"] = 842, -- Chitin Accumulator
+  ["54207"] = 842, -- Chitin Accumulator
+  ["54208"] = 842, -- Chitin Accumulator
+  ["54209"] = 842, -- Chitin Accumulator
+  ["54210"] = 842, -- Chitin Accumulator
+  ["54211"] = 842, -- Chitin Accumulator
+  ["54212"] = 842, -- Chitin Accumulator
+  ["54213"] = 846, -- Dwarven Secrets Gatherer
+  ["54214"] = 846, -- Dwarven Secrets Gatherer
+  ["54215"] = 843, -- Nature Collector
+  ["54216"] = 843, -- Nature Collector
+  ["54217"] = 843, -- Nature Collector
+  ["54218"] = 843, -- Nature Collector
+  ["54219"] = 843, -- Nature Collector
+  ["54220"] = 843, -- Nature Collector
+  ["54221"] = 843, -- Nature Collector
+  ["54222"] = 844, -- Monstrous Component Collector
+  ["54223"] = 844, -- Monstrous Component Collector
+  ["54224"] = 844, -- Monstrous Component Collector
+  ["54225"] = 844, -- Monstrous Component Collector
+  ["54226"] = 844, -- Monstrous Component Collector
+  ["54227"] = 844, -- Monstrous Component Collector
+  ["54228"] = 844, -- Monstrous Component Collector
+  ["54229"] = 847, -- Atronach Element Collector
+  ["54230"] = 847, -- Atronach Element Collector
+  ["54231"] = 847, -- Atronach Element Collector
+  ["54232"] = 847, -- Atronach Element Collector
+  ["54233"] = 848, -- Oblivion Shard Gatherer
+  ["54234"] = 848, -- Oblivion Shard Gatherer
+  ["54235"] = 848, -- Oblivion Shard Gatherer
+  ["54236"] = 848, -- Oblivion Shard Gatherer
+  ["54237"] = 848, -- Oblivion Shard Gatherer
+  ["54338"] = 838, -- Tamriel Beast Collector
   ---- Holidays
   -- ["147658"] = 2464, -- Festive Noise Maker (has progress, 0/10 bugged?)
   -- ["147659"] = 2465, -- Jester's Festival Joke Popper (has progress)
@@ -38,7 +90,29 @@ this.addonData = {
   ---- Gold Road
   ["207814"] = 4081, -- Silorn
   ["207815"] = 4081, -- Leftwheal Trading Post
+  ["207970"] = { 4055, 4056, 4149 }, -- 10/20/30 Mosaic Skill Shred
+  ---- Skill Stylist
+  ["214309"] = 4294, -- 10 Harvested Soul Fragments / Soul Trap
+  ---- Solstice
+  ["217925"] = 4446, -- 25 Phials of Tainted Blood / Eviscerate
 }
+
+function this:GetAchievementId(itemId)
+    local ID = AddonData[tostring(itemId)]
+    if ID == nil then return end
+    if type(ID) == "table" then
+      local uncomplitedIndex = 1
+      for index, achievementId in ipairs(ID) do
+        local _, _, _, _, completed = GetAchievementInfo(achievementId)
+        if completed then
+          uncomplitedIndex = index + 1
+        end
+      end
+      return ID[uncomplitedIndex], uncomplitedIndex, #ID
+    else
+      return ID
+    end
+end
 
 
 EVENT_MANAGER:RegisterForEvent(this.name, EVENT_ADD_ON_LOADED, function(event, addonName)
@@ -59,7 +133,7 @@ function this:init()
 
       local bagId, slotIndex = ZO_Inventory_GetBagAndIndex(inventorySlot)
       local itemId = GetItemId(bagId, slotIndex)
-      local achievementId = this.addonData[tostring(itemId)]
+      local achievementId = this:GetAchievementId(itemId)
       if achievementId then
         slotActions:AddCustomSlotAction(SI_DYEING_SWATCH_VIEW_ACHIEVEMENT, function()
           SCENE_MANAGER:HideCurrentScene()
@@ -78,24 +152,33 @@ function this:init()
 
   local function AddTooltipInfo(control, itemLink)
     local itemId = GetItemLinkItemId(itemLink)
-    local achievementId = this.addonData[tostring(itemId)]
+    local achievementId = this:GetAchievementId(itemId)
     if achievementId then
       local name, _, _, _, completed = GetAchievementInfo(achievementId)
       local criterions = GetAchievementNumCriteria(achievementId)
       local r, g, b = ZO_TOOLTIP_DEFAULT_COLOR:UnpackRGB()
 
+      local isList = false
       local texts = {}
       for i = 1, GetAchievementNumCriteria(achievementId) do
         local criterionDesctiprion, criterionNumCompleted, criterionNumRequired = GetAchievementCriterion(achievementId, i)
         local isCriterionComplete = criterionNumCompleted >= criterionNumRequired
-        table.insert(texts, criterionDesctiprion .. ": " .. ColorText(isCriterionComplete and "2ADC22" or "FFFFFF", criterionNumCompleted) .. "/" .. criterionNumRequired)
+
+        isList = criterionNumRequired == 1
+        -- table.insert(texts, criterionDesctiprion .. ": " .. ColorText(isCriterionComplete and "2ADC22" or "FFFFFF", criterionNumCompleted) .. "/" .. criterionNumRequired)
+        if isList then
+          table.insert(texts, ColorText(isCriterionComplete and ZO_TOOLTIP_DEFAULT_COLOR:ToHex() or ZO_DISABLED_TEXT:ToHex(), criterionDesctiprion))
+        else
+          table.insert(texts, criterionDesctiprion .. ": " .. ColorText(isCriterionComplete and UNLOCKED_COLOR:ToHex() or ZO_WHITE:ToHex(), criterionNumCompleted) .. "/" .. criterionNumRequired)
+        end
       end
 
       control:AddVerticalPadding(20)
       control:AddLine(GetString(SI_GROUPFINDERPLAYSTYLE8), "ZoFontWinH5", r, g, b, CENTER, MODIFY_TEXT_TYPE_NONE, CENTER, false)
       control:AddVerticalPadding(-10)
       control:AddLine(zo_strformat(name), "ZoFontHeader2", 1, 1, 1, CENTER, MODIFY_TEXT_TYPE_NONE, TEXT_ALIGN_CENTER, true)
-      control:AddLine(table.concat(texts, "\n"), "", r, g, b, CENTER, MODIFY_TEXT_TYPE_NONE, TEXT_ALIGN_CENTER, true)
+      -- control:AddLine(table.concat(texts, "\n"), "", r, g, b, CENTER, MODIFY_TEXT_TYPE_NONE, TEXT_ALIGN_CENTER, true)
+      control:AddLine(table.concat(texts, isList and ", " or "\n"), isList and "ZoFontWinH5" or "", r, g, b, CENTER, MODIFY_TEXT_TYPE_NONE, TEXT_ALIGN_CENTER, true)
     end
   end
 
@@ -118,6 +201,9 @@ function this:init()
     end)
     ZO_PostHook(ItemTooltip, "SetStoreItem", function(control, slotIndex)
       callback(control, GetStoreItemLink(slotIndex))
+    end)
+    ZO_PostHook(ItemTooltip, "SetQuestReward", function(control, rewardIndex)
+      callback(control, GetQuestRewardItemLink(rewardIndex))
     end)
     ZO_PostHook(ItemTooltip, "SetTradingHouseItem", function(control, slotIndex)
       callback(control, GetTradingHouseSearchResultItemLink(slotIndex))
